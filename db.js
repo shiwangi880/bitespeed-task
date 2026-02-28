7d420773c43e063a0e1d6dbe86a64b2f660d0697
shiwangi880 <shiwangik078@gmail.com>
2026-02-28 16:38:36 +0530

    Fix DB config using DATABASE_URL

--- db.js
+++ db.js
@@ -1,13 +1,11 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-const isProduction = process.env.DATABASE_URL?.includes("render.com");
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isProduction
-    ? { rejectUnauthorized: false }
-    : false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
